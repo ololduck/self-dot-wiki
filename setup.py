59ce6b06dd7ef6416e6d61d44b9ca5a62f8c65fb
Paul Ollivier <pollivier@slimpay.com>
2018-11-30 21:28:36 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name='self.wiki',
-        version='0.2.0',
+        version='0.3.0',
         packages=['self_wiki'],
         url='https://vit.am/paulollivier/self-dot-wiki',
         license='Do Whatever You Want',
